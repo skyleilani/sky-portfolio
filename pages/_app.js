456b9d48699fd4c25d8c704e78a101134f367b4d
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-12-13 13:40:05 -0800

    set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/Home.module.css';
+import Head from 'next/head';
+
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,400&family=Raleway:wght@300&family=Sora:wght@100;200&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/projects.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+import { tf } from '../public/tf.png';
+import Link from 'next/link';
+import ProjectCard from '../components/ProjectCard';
+
+const projects = () => {
+  return (
+    <div className='w-full'>
+      <div className='max-w-[11240px] mx-autho px-2 py-16'>
+        <p className='text-md tracking-widest uppercase text-red-400'>
+          Projects
+        </p>
+        <h2 className='py-4 italic mb-2'> What I've Built </h2>
+        <div className='grid sm:grid-cols-3 gap-10'>
+          <ProjectCard
+            title='Portfolio'
+            tech='Next.js, React, TailwindCSS'
+            src='https://github.com/skyleilani/equation-game-and-main-page-'
+            preview='https://o41qk.csb.app/'
+            img=''
+          />
+          <ProjectCard
+            title='Equation Game'
+            tech='React, CSS'
+            src='https://github.com/skyleilani/equation-game-and-main-page-'
+            preview='https://o41qk.csb.app/'
+            img='/../public/tf.png'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default projects;
