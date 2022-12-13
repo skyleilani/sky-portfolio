3aefb01f9732d22c5735bab8f06462e8fb60b6b9
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-12-13 13:47:13 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,20 @@ import Head from 'next/head';
 import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,400&family=Raleway:wght@300&family=Sora:wght@100;200&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
