e775af83f487646095f840d2c358ef3c693e4ace
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-12-13 13:13:38 -0800

    Create certificates.js

---
+++ pages/certificates.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const certificates = () => {
+  return (
+    <div className='grid grid-cols-5 gap-4'>
+      <div> 01 </div>
+      <div> 01 </div>
+      <div> 01 </div>
+      <div> 01 </div>
+      <div> 01 </div>
+      <div> 01 </div>
+    </div>
+  );
+};
+export default certificates;
