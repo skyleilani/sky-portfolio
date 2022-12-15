657af322413fdf7de3b9b5e9c3eb623086ca9354
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-12-14 16:59:10 -0800

    save state
    save state as I work through a new component

--- components/pages/Main.jsx
+++ components/pages/Main.jsx
@@ -11,7 +11,8 @@ const Main = () => {
       <div className='max-w[1240px] w-full h-full flex justify-center items-center'>
         <div className='mb-20'>
           <p className='uppercase text-md tracking-widest text-gray-600'>
-            {`Let's `}<span className='text-green-500'>Grow</span> Together
+            {`Let's `}
+            <span className='text-green-500'>Grow</span> Together
           </p>
           <h1 className='py-4 text-gray-700 '> {`Hi, I'm `}</h1>
           <span id={styles.color_animation} className='text-4xl tracking-wide '>
@@ -27,7 +28,7 @@ const Main = () => {
             focused on creating human-centered designs, and thoughtful
             solutions.
           </p>
-          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-5 my-5'>
             <div className='rounded-full shadow-md shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
               <FaLinkedinIn />
             </div>
