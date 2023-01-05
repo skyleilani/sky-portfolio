46c48492647fdbe1d418600c67375228c09c9c56
sky leilani <45158875+skyleilani@users.noreply.github.com>
2023-01-05 02:00:05 -0800

    Update ProjectCard.jsx

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -26,27 +26,24 @@ const ProjectCard = ({
       <div className='flex z-10 items-center'>
         {/* shift element down by 15px when element is tapped */}
         <div className=' w-full pl-10'>
-        <motion.div
-          whileTap={{ y: 15 }}
-          onClick={toggleTabs}
-          className='border w-full sm:w-4/6 cursor-pointer rounded-xl shadow-lg p-10 pb-20 m-5 w-full sm:w-9/12 bg-stone-100 hover:bg-stone-200 hover:shadow-2xl mx-auto'
-        >
-          
+          <motion.div
+            whileTap={{ y: 15 }}
+            onClick={toggleTabs}
+            className='border w-full sm:w-4/6 cursor-pointer rounded-xl shadow-lg p-10 pb-20 m-5 w-full sm:w-9/12 bg-stone-100 hover:bg-stone-200 hover:shadow-2xl mx-auto'
+          >
             <div className='w-full tracking-wide'>
               <h1 className='mb-5 text-md sm:text-lg'>{title}</h1>
               {/* show description on large screens, hide on small screens */}
               <p className='hidden sm:block'>{description}</p>
 
               {/* show the mobile description on small screens and hide on large screens */}
-              <p className='block sm:hidden text-sm italic'>{mobileDescription}</p>
-            
+              <p className='block sm:hidden text-sm italic'>
+                {mobileDescription}
+              </p>
             </div>
-         
-        </motion.div>
-        
-      </div>
-        <div className='flex flex-col border'>
-          
+          </motion.div>
+        </div>
+        <div className='flex flex-col '>
           <a
             className='border my-1 mb-10 p-2 shadow-md rounded-lg cursor-pointer bg-stone-100 hover:bg-stone-300 hover:translate-x-0.5'
             href={sourceCodeLink}
@@ -57,19 +54,18 @@ const ProjectCard = ({
           >
             {`://`}
           </a>
-      </div>
+        </div>
       </div>
 
       {/* If isTabsOpen is true, render the Tabs component */}
       {isTabsOpen && (
         // set initial position to 100% above normal position & completely transparent on first render
         // set animation target position to 0 and opacity to full
-      
+
         <motion.div
           initial={{ y: '-100%', opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ ease: 'linear', duration: 1 }}
-         
         >
           <Tabs
             onClose={toggleTabs}
@@ -78,11 +74,9 @@ const ProjectCard = ({
             expand={isTabsOpen}
             setExpand={setIsTabsOpen}
             tabDescription={tabDescription}
-              moreTabDescription={moreTabDescription}
-              
+            moreTabDescription={moreTabDescription}
           />
-          </motion.div>
-         
+        </motion.div>
       )}
     </div>
   );
